dd790860735d475f426b23f1ea1f54d4e487d150
RavinderSinghInfostride <ravinder.singh@infostride.com>
2022-09-09 16:56:21 +0530

    Order status change

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -3,10 +3,6 @@ package Pages;
 import net.jodah.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class LoginPage {
     WebDriver driver;
--- src/test/java/Pages/PageFactory.java
+++ src/test/java/Pages/PageFactory.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class PageFactory {
     WebDriver driver;
     private LoginPage loginPage;
+    private OrdersPage ordersPage;
 
     public PageFactory(WebDriver driver) {
         this.driver = driver;
@@ -15,5 +16,12 @@ public class PageFactory {
             loginPage = new LoginPage(driver);
         }
         return loginPage;
+    }
+
+    public OrdersPage getOrdersPage() {
+        if (ordersPage == null) {
+            ordersPage = new OrdersPage(driver);
+        }
+        return ordersPage;
     }
 }
